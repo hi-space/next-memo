0aa24949a13201fada42b359d9078fea51395f97
hi-space <hispace.j@gmail.com>
2024-11-08 23:40:27 +0900

    viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,18 @@
+import type { Metadata } from "next";
 import ThemeRegistry from "@/theme/provider";
 import { Providers } from "@/store/provider";
+
+export const metadata: Metadata = {
+  title: "Next Memo",
+  description: "Next Memo Application",
+};
+
+export const generateViewport = () => ({
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: "no",
+});
 
 export default function RootLayout({
   children,
